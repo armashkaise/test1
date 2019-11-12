0f641e6de907f656d4bb83956f31da7911502c40
armashkaise <HT52JJu7bv>
2019-11-12 17:16:43 +0600

    Commit #2

---
+++ src/Zadanie/Main.java
@@ -0,0 +1,18 @@
+package Zadanie;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean isTrue = true;
+        int i = 0;
+
+        if (isTrue) {
+            i += 10;
+            System.out.println(i);
+        } else {
+            i -= 10;
+            System.out.println(i);
+        }
+
+        System.out.println(i);
+    }
+}
