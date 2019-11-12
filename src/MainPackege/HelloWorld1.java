74e08d2a272d1c344a794d8aba707f980d527687
armashkaise <HT52JJu7bv>
2019-11-12 13:56:06 +0600

    Commit #1

---
+++ src/MainPackege/HelloWorld1.java
@@ -0,0 +1,8 @@
+package MainPackege;
+
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
